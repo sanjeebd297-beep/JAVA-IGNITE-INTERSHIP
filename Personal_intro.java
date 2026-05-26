34bc8686d9e04e1a5b57f39cf4f0380716c40f67
sanjeeb 25bcsa50 <sanjeebd297@gmail.com>
2026-05-26 22:59:17 +0530

    Add files via upload

---
+++ Personal_intro.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package Java_internship_assignment;
+
+/**
+ * 
+ */
+public class Personal_intro {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("NAME : Sanjeeb Dash");
+        System.out.println("Branch : CSE");
+        System.out.println("HOBBIES : Playing cricket");
+	}
+
+}
