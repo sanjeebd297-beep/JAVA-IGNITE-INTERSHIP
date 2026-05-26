c50effa44afa11d4aad17e056fc63e624c1683fd
sanjeeb 25bcsa50 <sanjeebd297@gmail.com>
2026-05-26 22:58:03 +0530

    Add files via upload

---
+++ Resturant_bill.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package Java_internship_assignment;
+
+/**
+ * 
+ */
+public class Resturant_bill {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Resturant name : ABC Hotel");
+        System.out.println("Food Item :");
+        System.out.println("1.friedrice:120");
+        System.out.println("2.Noodles:100");
+        System.out.println("3.Icecream : 100");
+        System.out.println("total amount:320");
+	}
+
+}
